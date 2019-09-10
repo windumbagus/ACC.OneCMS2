14fd4c92a87e8e0e60cc1e4c1463482fa4d31d5d
fawzanbawahab <49187650+fawzanbawahab@users.noreply.github.com>
2019-09-10 14:59:56 +0700

    commit

--- app/Http/Controllers/PendingController.php
+++ app/Http/Controllers/PendingController.php
@@ -8,6 +8,18 @@ class PendingController extends Controller
 {
     public function index()
     {
-        return view('pending');
+        //API
+        $url = "https://acc-dev1.outsystemsenterprise.com/ACCWorldCMS/rest/PendingListAPI/GetAllPendingList"; 
+        $ch = curl_init($url);                                                     
+        // curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));  
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");                                                            
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                  
+        $result = curl_exec($ch);
+        $err = curl_error($ch);
+        curl_close($ch);
+        $data = json_decode($result);
+        // dd($data);
+        
+        return view('pending',['Pendings' => $data]);    
     }
 }
