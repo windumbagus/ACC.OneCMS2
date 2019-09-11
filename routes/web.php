977765801cd09787583245b1a795e219646e45ad
fawzanbawahab <49187650+fawzanbawahab@users.noreply.github.com>
2019-09-11 14:01:24 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -28,5 +28,3 @@ Route::get('/approve/show','ApproveController@show');
 Route::get('/pendinglist','PendinglistController@index');
 Route::get('/pendinglist/update','PendinglistController@update');
 
-// coba coba
-
