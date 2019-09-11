da3e99293e7ba6dba567bfdb336ba0f0c52e8878
Windu M Bagus <windumbagus@gmail.com>
2019-09-11 09:06:48 +0700

    page approve

--- resources/views/approve.blade.php
+++ resources/views/approve.blade.php
@@ -6,8 +6,199 @@
 
 
 @section('content')
-<h3 class="box-title">Approve List</h3>
+<!-- TableUserCMS -->
+<div class="box box-primary">
+    <div class="box-header with-border">
+        <h3 class="box-title">Approve List</h3>
+    </div>
+    <!-- /.box-header -->
+    <div class="box-body">
 
+        <div class="row">
+                <div class="col-sm-8">
+                    <input type="text" placeholder="Search by Name, Username, Email or Mobile Phone" class="InputSearch form-control">
+                </div>
+                <div class="col-sm-4">
+                    <div class="col-sm-6">
+                        <a href="#" class="ButtonSearch btn btn-block btn-info">Search</a>    
+                    </div>
+                    <div class="col-sm-6">
+                        <a href="#" class="ResetSearch btn btn-block btn-info">Reset</a>    
+                    </div>
+                </div>
+            </div><br>
 
+        <table id="example1" class="table table-bordered display nowrap" style="width:100%">
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>User Name</th>
+            <th>Email</th>
+            <th>Mobile Phone</th>
+            <th>Action</th>
+        </tr>
+        </thead>
+        <tbody>
+            @foreach ($Approved as $Approve)
+
+            <tr>  
+                @if (property_exists($Approve->User, 'Name'))
+                    <td><span>{{$Approve->User->Name}}</span></td>
+                    @else 
+                    <td></td>
+                    @endif
+
+                    @if (property_exists($Approve->User, 'Username'))
+                    <td><span>{{$Approve->User->Username}}</span></td>
+                    @else 
+                    <td></td>
+                    @endif
+
+                    @if (property_exists($Approve->User, 'Email'))
+                    <td><span>{{$Approve->User->Email}}</span></td>
+                    @else 
+                    <td></td>
+                    @endif
+
+                    @if (property_exists($Approve->User, 'MobilePhone'))
+                    <td><span>{{$Approve->User->MobilePhone}}</span></td>
+                    @else 
+                    <td></td>
+                    @endif
+                <td>
+                <span>
+                    <a href="#" data-id="{{ $Approve->User->Id}}" class="view-approved btn btn-info btn-sm"><i class="fa fa-eye"></i></a> 
+                </span>
+                </td>
+            </tr>   
+                            
+            @endforeach       
+        </tbody>
+        </table>
+    </div>
+</div>
+
+ <!-- page script -->
+<script>
+    $(document).ready(function () {
+        $('#example1').DataTable({
+            'deferRender': true,
+            'paging'      : true,
+            'lengthChange': false,
+            'searching'   : true,
+            'ordering'    : true,
+            'info'        : true,
+            'autoWidth'   : true,
+            'scrollX': true,
+            sDom: 'lrtip', 
+            "columns": [
+                null,
+                null,
+                null,
+                null,
+                {"searchable":false},
+            ]
+        })
+
+        //Button Search
+        $('.ButtonSearch').on('click', function(){
+            var searchData = $('.InputSearch').val()
+            var dtable = $('#example1').DataTable()
+            dtable.search(searchData).draw()
+        })
+
+        //Reset Button Search
+        $('.ResetSearch').on('click',function(){
+            var tab = $('#example1').DataTable()
+            tab.search('').draw()
+            $('.InputSearch').val('')
+        })
+
+        //VIEW
+        $(document).on('click','.view-approved',function(){
+            var id = $(this).attr('data-id');
+            console.log(id);
+            $.ajax({
+                url:"{{asset('/approve/show')}}",
+                data: {'Id':id ,'_token':'{{csrf_token()}}' },
+                dataType:'JSON', 
+                type:'GET',
+                success: function (val){
+                    console.log(val);
+                    $('[name="approve_Name_view"]').val(val.User.Name);
+                    $('[name="approve_Username_view"]').val(val.User.Username);
+                    $('[name="approve_Email_view"]').val(val.User.Email);
+                    $('[name="approve_MobilePhone_view"]').val(val.User.MobilePhone);
+                    $('[name="approve_Last_Login_view"]').val(val.User.Last_Login);
+                    $('[name="approve_Is_Active_view"]').val(val.User.Is_Active);
+                    $('[name="approve_TanggalLahir_view"]').val(val.MstCustomerDetail.TanggalLahir);
+                    $('[name="approve_Status_view"]').val(val.MstStatus.Label);
+                    //StatusNoHP
+                    if (val.MstCustomerDetail.hasOwnProperty('StatusNoHP')) {
+                        $('[name="approve_StatusNoHP_view"]').val(val.MstCustomerDetail.StatusNoHP);   
+                    }else{
+                        $('[name="approve_StatusNoHP_view"]').val("False");        
+                    }
+                    // Subscribe
+                    if (val.MstCustomerDetail.hasOwnProperty('Subscribe')) {
+                        $('[name="approve_Subscribe_view"]').val(val.MstCustomerDetail.Subscribe);   
+                    }else{
+                        $('[name="approve_Subscribe_view"]').val("N");        
+                    }
+
+                    // FotoProfil
+                    if (val.FotoProfil_MstPicture.hasOwnProperty('Picture')) {
+                        $('[name="approve_Profile_view"]').attr("src","data:image/jpeg;base64,"+val.FotoProfil_MstPicture.Picture);   
+                    }else{
+                        $('[name="approve_Profile_view"]').val("");        
+                    }
+                    
+                    // FotoKTP
+                    if (val.hasOwnProperty('FotoKTP_MstPictures')) {
+                        if (val.FotoKTP_MstPictures.hasOwnProperty('Picture')) {
+                            $('[name="approve_KTP_view"]').attr("src","data:image/jpeg;base64,"+val.FotoKTP_MstPictures.Picture);   
+                        }else{
+                            $('[name="approve_KTP_view"]').val("");        
+                        }
+                    }else{
+
+                    }
+
+                    // FotoNPWP
+                    if (val.hasOwnProperty('FotoNPWP_MstPictures')) {
+                        if (val.FotoNPWP_MstPictures.hasOwnProperty('Picture')) {
+                            $('[name="approve_NPWP_view"]').attr("src","data:image/jpeg;base64,"+val.FotoNPWP_MstPictures.Picture);   
+                        }else{
+                            $('[name="approve_NPWP_view"]').val("");        
+                        }
+                    }else{
+
+                    }
+
+                    // FotoKK
+                    if (val.hasOwnProperty('FotoKK_MstPictures')) {
+                        if (val.FotoKK_MstPictures.hasOwnProperty('Picture')) {
+                            $('[name="approve_KK_view"]').attr("src","data:image/jpeg;base64,"+val.FotoKK_MstPictures.Picture);   
+                        }else{
+                            $('[name="approve_kk_view"]').val("");        
+                        }
+                    }else{
+
+                    }
+
+                },
+                error: function( jqXhr, textStatus, errorThrown ){
+                console.log(jqXhr);
+                console.log( errorThrown );
+                console.log(textStatus);
+                },
+            });
+            $('#view-approve').modal();
+        });
+
+        
+    })
+</script>
+@include('modal.view_approve')
 @endsection 
 
--- resources/views/modal/view_approve.blade.php
+++ resources/views/modal/view_approve.blade.php
@@ -1,5 +1,5 @@
 <!-- Modal ADD -->
-<div class="modal fade" id="view-rejected" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" data-keyboard="false" data-backdrop="static">
+<div class="modal fade" id="view-approve" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" data-keyboard="false" data-backdrop="static">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header"></div>
@@ -8,83 +8,78 @@
                     <div class="box-header">
                         <i class="fa fa-database"></i>
                 
-                        <h3 class="box-title">View Rejected</h3>
+                        <h3 class="box-title">View approve</h3>
                         <!-- tools box -->
                         <!-- /. tools -->
                     </div>
                     <div class="box-body">
-                        <form id="form-view-rejected" action="#" method="post"> 
+                        <form id="form-view-approve" action="#" method="post"> 
                         @csrf	
                         <div class="form-group">
                             <label>Foto Profile:</label><br>
-                            <img style="width: 150px; height: 200px;" name="rejected_Profile_view" alt=""/><br>
+                            <img style="width: 150px; height: 200px;" name="approve_Profile_view" alt=""/><br>
                         </div>
                         <div class="form-group">
                             <label>Name :</label>
-                            <input type="text" class="form-control" name="rejected_Name_view"
+                            <input type="text" class="form-control" name="approve_Name_view"
                             placeholder="Name">
                         </div>
                         <div class="form-group">
                             <label>Username :</label>
-                            <input type="text" class="form-control" name="rejected_Username_view"
+                            <input type="text" class="form-control" name="approve_Username_view"
                             placeholder="Username">
                         </div>
                         <div class="form-group">
                             <label>Email:</label>
-                            <input type="text" class="form-control" name="rejected_Email_view"
+                            <input type="text" class="form-control" name="approve_Email_view"
                             placeholder="Email">
                         </div>
                         <div class="form-group">
                             <label>Mobile Phone:</label>
-                            <input type="text" class="form-control" name="rejected_MobilePhone_view"
+                            <input type="text" class="form-control" name="approve_MobilePhone_view"
                             placeholder="Mobile Phone">
                         </div>
                         <div class="form-group">
                             <label>Last Login:</label>
-                            <input type="text" class="form-control" name="rejected_Last_Login_view"
+                            <input type="text" class="form-control" name="approve_Last_Login_view"
                             placeholder="Last Login">
                         </div>
                         <div class="form-group">
                             <label>Is Active:</label>
-                            <input type="text" class="form-control" name="rejected_Is_Active_view"
+                            <input type="text" class="form-control" name="approve_Is_Active_view"
                             placeholder="Is Active">
                         </div>
                         <div class="form-group">
                             <label>Tanggal Lahir:</label>
-                            <input type="text" class="form-control" name="rejected_TanggalLahir_view"
+                            <input type="text" class="form-control" name="approve_TanggalLahir_view"
                             placeholder="Tanggal Lahir">
                         </div>
                         <div class="form-group">
                             <label>Status:</label>
-                            <input type="text" class="form-control" name="rejected_Status_view"
+                            <input type="text" class="form-control" name="approve_Status_view"
                             placeholder="Status">
                         </div>
                         <div class="form-group">
                             <label>Status No HP:</label>
-                            <input type="text" class="form-control" name="rejected_StatusNoHP_view"
+                            <input type="text" class="form-control" name="approve_StatusNoHP_view"
                             placeholder="StatusNoHP">
                         </div>
                         <div class="form-group">
                             <label>Subscribe:</label>
-                            <input type="text" class="form-control" name="rejected_Subscribe_view"
+                            <input type="text" class="form-control" name="approve_Subscribe_view"
                             placeholder="Subscribe">
                         </div>
                         <div class="form-group">
                             <label>Foto KTP:</label><br>
-                            <img style="width: 150px; height: 200px;" name="rejected_KTP_view" alt=""/><br>
+                            <img style="width: 150px; height: 200px;" name="approve_KTP_view" alt=""/><br>
                         </div>
                         <div class="form-group">
                             <label>Foto NPWP:</label><br>
-                            <img style="width: 150px; height: 200px;" name="rejected_NPWP_view" alt=""/><br>
+                            <img style="width: 150px; height: 200px;" name="approve_NPWP_view" alt=""/><br>
                         </div>
                         <div class="form-group">
                             <label>Foto KK:</label><br>
-                            <img style="width: 150px; height: 200px;" name="rejected_KK_view" alt=""/><br>
-                        </div>
-                        <div class="form-group">
-                            <label>Reason:</label>
-                            <input type="text" class="form-control" name="rejected_Reason_view"
-                            placeholder="Reason">
+                            <img style="width: 150px; height: 200px;" name="approve_KK_view" alt=""/><br>
                         </div>
                     </div>
                 </div>
@@ -100,8 +95,8 @@
 <Script>
     $(function() {
         $('#close-modal').click(function() {
-        $('#view-rejected').modal('hide');
-        $('#form-view-rejected')[0].reset();  
+        $('#view-approve').modal('hide');
+        $('#form-view-approve')[0].reset();  
         });      
     });
 </Script>
