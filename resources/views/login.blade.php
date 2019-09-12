c082a19cbb96ab2a512ded99909bc546ba4c51f3
Windu M Bagus <windumbagus@gmail.com>
2019-09-12 09:47:29 +0700

    login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <link rel="shortcut icon" href="assets/ACC_logo.ico"> 
-  <title>CMS-LMS ACC | Login</title>
+  <title>ACC.One CMS | Login</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
@@ -75,7 +75,7 @@
 
   </div>
   <!-- /.login-box-body -->
-  <p style="margin-top:10%" class="login-box-msg"><strong>Copyright &copy; 2019 <a href="#">WMB & ISRD</a>.</strong>  All rights reserved.</p>
+  <p style="margin-top:10%" class="login-box-msg"><strong>Copyright &copy; 2019 <a href="#">WMB & FZN</a>.</strong>  All rights reserved.</p>
 </div>
 <!-- /.login-box -->
 
