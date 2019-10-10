d45c22b640fd8260da2c3b5e12ad4f651cbab217
Windu M Bagus <windumbagus@gmail.com>
2019-10-10 16:40:23 +0700

    Page user cms

--- app/Http/Controllers/UserCMSController.php
+++ app/Http/Controllers/UserCMSController.php
@@ -53,4 +53,59 @@ class UserCMSController extends Controller
  
         return json_encode($val);
     }
+
+    // public function update(Request $request)
+    // {
+    //     // if($request->Is_Active == "on"){
+    //     //     $Is_Active = True; 
+    //     // }else{
+    //     //     $Is_Active = False;
+    //     // }
+
+    //     $data = json_encode(array(
+    //         "User" => array(   
+    //             "Id"=> $request->Id_update,
+    //             "Name"=>$request->Name_update,
+    //             "Username"=>$request->Username_update,
+    //             "Password"=>$request->Password_update,
+    //             "Email"=>$request->Emal_update,
+    //             "MobilePhone"=>$request->MobilePhone_update,
+    //             "External_Id"=>$request->External_Id_update,
+    //             "Creation_Date"=>$request->Creation_Date_update,
+    //             "Last_Login"=>$request->Last_Login_update,
+    //             "Is_Active"=>$request->Is_Active_update,
+    //         ),
+    //         "UserDetail" => array(
+    //             "Id"=>$request->,
+    //             "UserId"=>$request->,
+    //             "RoleId"=>$request->,
+    //             "UserCategory"=>$request->,
+    //             "IsSuperAdmin"=>$request->,
+    //             "AddedDate"=>$request->,
+    //             "UserAdded"=>$request->,
+    //             "UpdatedDate"=>$request->,
+    //             "UserUpdated"=>$request->,
+    //             "Status"=>$request->,
+    //             "Organization"=>$request->,
+    //             "NPK"=>$request->,
+    //             "ExpiredDate"=>$request->,
+    //             "Address"=>$request->,
+    //             "Supplier"=>
+    //         ),
+    //     )); 
+    //     // dd($data);
+    //     $url = ""; 
+    //     $ch = curl_init($url);                   
+    //     curl_setopt($ch, CURLOPT_POST, true);                                  
+    //     curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    //     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);   
+    //     curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));                                                             
+    //     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                  
+    //     $result = curl_exec($ch);
+    //     $err = curl_error($ch);
+    //     curl_close($ch);
+    //     $Hasils= json_decode($result); 
+    //     // dd($Hasils);
+    //         return redirect("user-cms")->with('success','Data User CMS Update Successfull !!!');
+    // }
 }
