93c7cf3a1d3fb0edee0de9b7ca05c186fcded8ce
Windu M Bagus <windumbagus@gmail.com>
2019-10-28 10:40:34 +0700

    check role isView

---
+++ resources/views/invalid_permission.blade.php
@@ -0,0 +1,5 @@
+@extends('admin.admin') 
+
+@section('content')
+<h1  style="text-align: center;">Invalid Permission to Load Page !!!</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -229,4 +229,5 @@ Route::group(['middleware' => ['SessionCheck','revalidate']],function()
     Route::get('/multipurpose/FollowUp','MultipurposeController@FollowUp');
     Route::get('/multipurpose/download/{Status}/{StartDate}~{EndDate}','MultipurposeController@download');
 
+    Route::get('/invalid-permission','InvalidPermissionController@index');
 });//end group route
