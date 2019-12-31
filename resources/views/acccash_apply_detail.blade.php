f3ca2a5870b044ce24f995b1ec52d065bda9b668
Windu M Bagus <windumbagus@gmail.com>
2019-12-31 16:36:53 +0700

    modal image

--- resources/views/acccash_apply_detail.blade.php
+++ resources/views/acccash_apply_detail.blade.php
@@ -5,6 +5,98 @@
 
 @section('content')
 
+{{-- css image --}}
+<style>
+	
+	body {font-family: Arial, Helvetica, sans-serif;}
+	
+	#myImg, #myImg2 {
+	  border-radius: 5px;
+	  cursor: pointer;
+	  transition: 0.3s;
+	}
+	
+	#myImg, #myImg2:hover {opacity: 0.7;}
+	
+	/* The Modal (background) */
+	.modal {
+	  display: none; /* Hidden by default */
+	  position: fixed; /* Stay in place */
+	  z-index: 1; /* Sit on top */
+	  padding-top: 100px; /* Location of the box */
+	  left: 22%;
+	  top: 0;
+	  width: 80%; /* Full width */
+	  height: 100%; /* Full height */
+	  overflow: auto; /* Enable scroll if needed */
+	  background-color: rgb(0,0,0); /* Fallback color */
+	  background-color: rgba(0,0,0,0.9); /* Black w/ opacity */
+	}
+	
+	/* Modal Content (image) */
+	.modal-content {
+	  margin: auto;
+	  display: block;
+	  width: 80%;
+	  max-width: 700px;
+	}
+	
+	/* Caption of Modal Image */
+	#caption {
+	  margin: auto;
+	  display: block;
+	  width: 80%;
+	  max-width: 700px;
+	  text-align: center;
+	  color: #ccc;
+	  padding: 10px 0;
+	  height: 150px;
+	}
+	
+	/* Add Animation */
+	.modal-content, #caption {  
+	  -webkit-animation-name: zoom;
+	  -webkit-animation-duration: 0.6s;
+	  animation-name: zoom;
+	  animation-duration: 0.6s;
+	}
+	
+	@-webkit-keyframes zoom {
+	  from {-webkit-transform:scale(0)} 
+	  to {-webkit-transform:scale(1)}
+	}
+	
+	@keyframes zoom {
+	  from {transform:scale(0)} 
+	  to {transform:scale(1)}
+	}
+	
+	/* The Close Button */
+	.close {
+	  position: absolute;
+	  top: 15px;
+	  right: 35px;
+	  color: #f1f1f1;
+	  font-size: 40px;
+	  font-weight: bold;
+	  transition: 0.3s;
+	}
+	
+	.close:hover,
+	.close:focus {
+	  color: #bbb;
+	  text-decoration: none;
+	  cursor: pointer;
+	}
+	
+	/* 100% Image Width on Smaller Screens */
+	@media only screen and (max-width: 700px){
+	  .modal-content {
+		width: 100%;
+	  }
+	}
+	</style>
+
 <!-- TableACCCashLog -->
 <div class="box box-primary">
     <div class="box-header with-border">
@@ -146,20 +238,25 @@
 												<div class="form-group">
 													<div class="col-sm-6">
 														@if(!empty($AccCashApplyPictures[0]->PATH_FILE))
-														<a href="{{$AccCashApplyPictures[0]->PATH_FILE}}">
-															<img style="width: 50%; height: 50%;" alt="" src="{{$AccCashApplyPictures[0]->PATH_FILE}}" class="center"/>
-														</a>
+															<img id="myImg" style="width: 50%; height: 50%;" alt="" src="{{$AccCashApplyPictures[0]->PATH_FILE}}" class="center"/>
 														@endif
 													</div>
 													
 													<div class="col-sm-6">
 														@if(!empty($AccCashApplyPictures[1]->PATH_FILE))
-														<a href="{{$AccCashApplyPictures[1]->PATH_FILE}}">
-															<img style="width: 50%; height: 50%;" alt="" src="{{$AccCashApplyPictures[1]->PATH_FILE}}" class="center"/>
-														</a>
+															<img id="myImg2" style="width: 50%; height: 50%;" alt="" src="{{$AccCashApplyPictures[1]->PATH_FILE}}" class="center"/>
 														@endif
 													</div>
+
+													<!-- The Modal image-->
+													<div id="myModal" class="modal">
+														<span class="close">&times;</span>
+														<img class="modal-content" id="img01">
+														<div id="caption"></div>
+													</div>
+
 												</div>
+												
 												@if ($Statusapply == "PENDING")
 												<div class="form-group">
 													<label class="col-sm-3 control-label">Status</label>
@@ -406,5 +503,37 @@
         });
 
     })
+
+	// Get the modal image
+	var modal = document.getElementById("myModal");
+
+	// Get the image and insert it inside the modal - use its "alt" text as a caption
+	var img = document.getElementById("myImg");
+	var modalImg = document.getElementById("img01");
+	var captionText = document.getElementById("caption");
+	img.onclick = function(){
+	modal.style.display = "block";
+	modalImg.src = this.src;
+	captionText.innerHTML = this.alt;
+	}
+
+	// Get the image and insert it inside the modal - use its "alt" text as a caption
+	var img2 = document.getElementById("myImg2");
+	var modalImg2 = document.getElementById("img01");
+	var captionText2 = document.getElementById("caption");
+	img2.onclick = function(){
+	modal.style.display = "block";
+	modalImg2.src = this.src;
+	captionText2.innerHTML = this.alt;
+	}
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() { 
+	modal.style.display = "none";
+	}
+
   </script>
 @endsection
